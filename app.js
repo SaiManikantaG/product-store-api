ed2fc461d1c1ca825d39ff4cfd850e52ad1d8fcf
SaiManikantaG <saig2304@gmail.com>
2018-04-16 18:56:15 -0500

    Changes to app.js file

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ var inventoryRouter = require('./routes/apis/inventory');
 var productListRouter = require('./routes/apis/productList');
 
 var app = express();
-
+// Enable cross origin
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
