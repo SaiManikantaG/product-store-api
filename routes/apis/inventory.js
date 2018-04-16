4228ab28765a5477c52d2e814385e36a9e8756e9
SaiManikantaG <saig2304@gmail.com>
2018-04-16 17:53:47 -0500

    Initial commit working directory

---
+++ routes/apis/inventory.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var inventoryList = require('../datasource/inventory.json');
+
+/* GET inventory listing. */
+router.get('/', function(req, res, body) {
+  res.status(200).send(inventoryList);
+});
+
+module.exports = router;
