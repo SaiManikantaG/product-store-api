1cee1aa2fe502bafd12f86d0e12862fcb053c891
SaiManikantaG <saig2304@gmail.com>
2018-04-16 22:02:08 -0500

    Revert Back

--- routes/apis/inventory.js
+++ routes/apis/inventory.js
@@ -1,35 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var inventoryList = require('../datasource/inventory.json');
-var request = require('request');
 
-var inventory = function() {
-	var url = 'http://autumn-resonance-1298.getsandbox.com/inventory';
-	request(
-		{
-			url: url,
-			json: true,
-		},
-		function(error, response, body) {
-			if (!error && response.statusCode === 200) {
-				//	productApi.inventoryApi(app, body);
-				var invent = body;
-				var content = JSON.stringify(body);
-				/* GET inventory listing. */
-				router.get('/', function(req, res, body) {
-					console.log(`inventory list obtained: ${JSON.stringify(invent)}`);
-					fs.writeFile('../datasource/inventory.json', content, 'utf8', function(err) {
-						if (err) {
-							return console.log(err);
-						}
-					});
-					res.status(200).send(invent);
-				});
-			}
-		}
-	);
-};
+/* GET inventory listing. */
+router.get('/', function(req, res, body) {
+	res.status(200).send(inventoryList);
+});
 
-inventory();
-module.exports = inventory;
 module.exports = router;
